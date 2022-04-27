95b8eaa26eabc90a7cb3ff8af3fe1ce47fc80d9b
Wesley Dias <wesd_444@hotmail.com>
2022-04-27 18:40:07 -0300

    renamed build file and added infolog before server run

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	app := server.New()
+	utils.InfoLog.Println("starting the server on port", os.Getenv("API_PORT"))
 	if err := app.Listen(os.Getenv("API_PORT")); err != nil {
 		utils.ErrorLog.Panic(err.Error())
 	}
